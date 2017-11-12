146dd775a018347cb49c8796d3be0b8f934e0538
jwang13 <jennywang01@college.harvard.edu>
2017-11-11 22:41:21 -0500

    More data cleaning

--- data/datacleaning.py
+++ data/datacleaning.py
@@ -1,22 +1,24 @@
 import csv
-year = 2014
+year = 1974
 congNum = 93
 with open('allCongressDataPublish.csv', 'r') as fin:
     reader = csv.reader(fin, delimiter='\t')
     with open('new_allCongressDataPublish.csv', 'w') as fout:
         writer = csv.writer(fout, delimiter='\t')
-        # set headers here, grabbing headers from reader first
         writer.writerow(next(reader) + ['Year'])
 
         for row in reader:
             rowSplit = row[0].split(',')
-            curCongNum = rowSplit[65] 
+            curCongNum = rowSplit[65]
             # if(curCongNum == "NA"):
             #     print rowSplit
-            if((curCongNum != "NA") and (curCongNum != congNum) and (curCongNum > 90)):
-            	print year
-                # print curCongNum
-            	year = year - 2
-            	congNum = curCongNum
-            row.append(year)
+            if(curCongNum == "NA"):
+                curCongNum = 0
+            curCongNum = int(curCongNum)
+            if((curCongNum != congNum) and (curCongNum > 90)):
+                print year
+                year = year + 2
+                congNum = curCongNum
+            row[0] = row[0] + "," + str(year)
+            # print row
             writer.writerow(row)
