4cda2aa33392f0571db774e3be0b1656d6fc5bef
jwang13 <jennywang01@college.harvard.edu>
2017-11-23 17:44:13 -0500

    Prettier charts

--- js/vis-district-charts.js
+++ js/vis-district-charts.js
@@ -37,7 +37,7 @@ function createLineCharts(error){
     // console.log(districtData)
     createRaceChart(districtData)
     createIncomeChart(districtData)
-    createEducationChart(districtData)
+    createEmploymentChart(districtData)
 
 }
 
@@ -55,24 +55,40 @@ function createRaceChart(districtData){
     raceData.push(prcntWhite)
     // raceData.push(other)
     // console.log(raceData)
+    chartsSvg.append("text")
+        .attr("class","race-title")
+        .style("fill", "black")
+        .attr("x", 20)
+        .attr("y", 16)
+        .text("Race");
+
     var races = ["Asian", "Black", "Hispanic", "White"]
 
     var raceChart = chartsSvg.append("g")
         .attr("class", "race")
+    
+    var dataRaceBars = raceChart.selectAll("rect")
+        .data(raceData, function(d) {
+            return d.key;
+        });
 
-    var raceBars = raceChart.selectAll("rect")
-        .data(raceData)
-        .enter().append("rect")
-        .attr("fill", "grey")
-        .attr("width", function(d) {
-            // console.log(d)
-            return d
-        })
+    d3.selectAll(".race-bars").remove();
+
+    var raceBars = dataRaceBars.enter()
+        .append("rect")
+        .attr('transform', 'translate(100, 50)')
+        .attr("class", "race-bars")
+        .attr("fill", "steelblue")
+
+    raceBars.merge(raceBars)
+        //.transition()
+        //.duration(1000)
+        .attr("width", function(d) {return d})
         .attr("height", 20)
-        .attr("x", 80)
+        .attr("x", 0)
         .attr("y", function(d, index) {
             return (index * 30);
-        });
+        })
 
     chartsSvg.selectAll("text.race")
         .data(races)
@@ -83,13 +99,13 @@ function createRaceChart(districtData){
         .attr("height", 10)
         .attr("x", 50)
         .attr("y", function(d, index) {
-            return (10+index * 30);
+            return (50+index * 30);
         })
         .text(function(d) {
             return d;
         });
 
-    raceBars.exit().transition(500).remove();
+    //raceBars.exit().transition(500).remove();
 }
 
 function createIncomeChart(districtData){
@@ -114,75 +130,110 @@ function createIncomeChart(districtData){
     incomeData.push(from75to100)
     incomeData.push(from100to150)
     incomeData.push(from150to200)
-    // console.log(incomeData)
+    console.log(incomeData)
     var incomes = ["under25k", "25to50k", "50to75k", "75to100k","100to150k","150to200k"];
 
     var incomeChart = chartsSvg.append("g")
         .attr("class", "income")
+        .attr("transform", "translate(200,30)")
+        .attr("width", 100)
+        .attr("height", 600);
 
-    // var incomeBars = incomeChart.selectAll("rect")
-    //     .data(incomeData)
-    //     .enter().append("rect")
-    //     .attr("fill", "steelblue")
-    //     .attr("title", "sdfj")
-    //     .attr("width", function(d) {
-    //         return d
-    //     })
-    //     .attr("height", 20)
-    //     .attr("x", 280)
-    //     .attr("y", function(d, index) {
-    //         return (10 + index * 30);
-    //     });
-    var dataIncomeBars = incomeChart.selectAll("rect")
-        .data(incomeData, function(d) {
-            // console.log(incomeData)
-            return d.key;
-        });
+    var x = d3.scaleLinear().domain([0,5]).range([0, 200]);
+    var y = d3.scaleLinear().domain([0,100]).range([200, 0]);
+    var xAxis = d3.axisBottom(x).ticks(6).tickFormat(function (d,i) {return incomes[i]});
+    var yAxis = d3.axisLeft(y);
 
-    var incomeBars = dataIncomeBars.enter()
-        .append("rect")
-        .attr('transform', 'translate(100, 0)')
-        .attr("class", "income-bars")
-        .attr("fill", "steelblue")
+    incomeChart.append('g')
+        .attr("transform", 'translate(50, 200)')
+        .attr("id", "xAxis")
+        .attr("class", "x axis")
+        .call(xAxis)
+        .selectAll("text")
+        .attr("y", 15)
+        .attr("x", -35)
+        .attr("dy", ".35em")
+        .attr("transform", "rotate(-60)")
+        //.style("text-anchor", "start");
+    incomeChart.append('g')
+        .attr("transform", 'translate(50, 0)')
+        .attr("id", "yAxis")
+        .attr("class", "y axis")
+        .call(yAxis);
 
-    incomeBars.merge(incomeBars)
-            .transition()
-            .duration(1000)
-            .attr("width", function(d) {return d})
-            .attr("height", 20)
-            .attr("x", 280)
-            .attr("y", function(d, index) {
-                    return (index * 30);
-                })
+    var incomeLine = d3.line()
+        .x(function(d,i) {
+            console.log(i)
+            return x(+i); })
+        .y(function(d) {
+            console.log(d)
+            return y(+d); });
 
-    incomeBars.exit().transition(500).remove();
+    d3.selectAll(".income-line").remove();
+
+    incomeChart.append("path")
+        .data([incomeData])
+        .attr("class", "income-line")
+        .attr("transform", 'translate(50, 0)')
+        .attr("d", incomeLine);
 
     chartsSvg.append("text")
+        .attr("class","income-title")
         .style("fill", "black")
-        .attr("font-size", "0px")
-        .attr("x", 250)
-        .attr("y", 50)
+        .attr("x", 260)
+        .attr("y", 16)
         .text("Income Distribution");
 
-    chartsSvg.selectAll("text.race")
-        .data(incomes)
-        .enter().append("text")
-        .attr("text-anchor", "end")
-        .style("fill", "black")
-        .attr("font-size", "10px")
-        .attr("height", 10)
-        .attr("x", 250)
-        .attr("y", function(d, index) {
-            return (10+index * 30);
-        })
-        .text(function(d) {
-            return d;
-        });
+    chartsSvg.append("text")
+        .attr("transform", "translate(280,310)")
+        .text("Annual Income Bracket");
+    chartsSvg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 205 )
+        .attr("x",-130)
+        .attr("dy", "1em")
+        //.attr("transform", "translate(40,0)")
+        .style("text-anchor", "middle")
+        .text("% of Total Population");
 
-    //incomeBars.exit().transition(500).remove();
 }
 
-function createEducationChart(districtData){
+function createEmploymentChart(districtData){
     console.log(districtData)
+    var unemp = districtData[0].prcntUnemp
+    var emData = []
+    emData.push(100-unemp)
+    emData.push(unemp)
+    console.log(unemp)
+    var color = ["#aedd56", "#f2b0d1"];
+    var arc = d3.arc().outerRadius(50).innerRadius(0);
+
+    var employmentChart = chartsSvg.append("g")
+        .attr("class", "employment")
+        .attr("transform", "translate(450,0)")
+
+    chartsSvg.append("text")
+        .attr("class","emp-title")
+        .style("fill", "black")
+        .attr("x", 500)
+        .attr("y", 16)
+        .text("Employment");
+
+    var pie = d3.pie()
+        .value(function(d,i) {
+            return d
+        }).sort(null);
+
+    var path = employmentChart.selectAll('path')
+        .data(pie(emData))
+        .enter().append('path')
+        .attr('d', arc)
+        .attr("transform", "translate(100,100)")
+        .attr('stroke', '#fff') // <-- THIS
+        .attr('stroke-width', '2') // <-- THIS
+        .attr('fill', function(d, i) {
+            console.log("sdf")
+            return color[i];
+        });
 
 }
