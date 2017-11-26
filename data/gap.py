80a695c061fa46bdcc74398a0112a5074abc7297
jwang13 <jennywang01@college.harvard.edu>
2017-11-26 16:20:57 -0500

    Efficiency gap algorithm update

--- data/gap.py
+++ data/gap.py
@@ -18,14 +18,17 @@ with open('2014.csv', 'r') as fin:
         gap = 0
         for row in reader:
             rowSplit = row[0].split(',')
-            print rowSplit
+            #print rowSplit
             if(rowSplit[0] != curState):
                 totalSeats = seatsR + seatsD
                 print totalSeats
                 seatMargin = max(seatsR/(1.0*totalSeats),seatsD/(1.0*totalSeats))-.5
                 voteMargin = max(votesR/(1.0*totalVotes),votesD/(1.0*totalVotes))-.5
+                print voteMargin
                 gap = seatMargin-2*voteMargin
                 gap = gap*100
+                if(gap < 0):
+                    gap = gap * -1
                 gap = "%0.2f" % gap
                 writer.writerow([curState,gap,totalSeats])
                 print curState
@@ -36,6 +39,20 @@ with open('2014.csv', 'r') as fin:
                 votesR = 0
                 votesD = 0
                 totalVotes = 0
+            if(int(rowSplit[5])==0):
+                # R win uncontested
+                print curState
+                votesD += round(int(rowSplit[6])*.25)
+                votesR += round(int(rowSplit[6])*.75)
+                totalVotes += round(int(rowSplit[6]))
+                seatsR += 1
+            if(int(rowSplit[6])==0):
+                # D win uncontested
+                print curState + "D"
+                votesD += round(int(rowSplit[5])*.75)
+                votesR += round(int(rowSplit[5])*.25)
+                totalVotes += round(int(rowSplit[5]))
+                seatsD += 1
             if(int(rowSplit[5])!=0 and int(rowSplit[6])!=0):
                 if "R" in rowSplit[4]:
                     seatsR += 1
@@ -52,4 +69,6 @@ with open('2014.csv', 'r') as fin:
                 seatMargin = .5
                 voteMargin = max(int(rowSplit[5])/(1.0*totalVotes),int(rowSplit[6])/(1.0*totalVotes))-.5
                 gap = seatMargin-2*voteMargin
+                gap = gap*100
+                gap = "%0.2f" % gap
                 writer.writerow([curState,gap,1])
