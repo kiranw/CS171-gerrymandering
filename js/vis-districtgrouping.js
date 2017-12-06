f4e7a7cac11648314b1d768b1b8792d74a1af189
Jenny Fan <fan.jenny@gmail.com>
2017-12-06 14:37:53 -0500

    Updated vis-districtgrouping.js so different grouping types work, need to fix exit/remove

--- js/vis-districtgrouping.js
+++ js/vis-districtgrouping.js
@@ -5,66 +5,52 @@ var selectedGroup, groupingSvg;
 var groupColor = d3.scaleOrdinal()
     .range(['#e66101','#fdb863','#f7f7f7','#b2abd2','#5e3c99']);
 
-var cellHeight = 40, cellWidth = 40;
-var cellPadding = 5;
+var cellHeight = 40, cellWidth = 40, cellPadding = 5;
 
-// var groupingTypes = [
-// { key: "party", title: "Party"},
-// { key: "perfectRep", title: "Perfect Representation"},
-// { key: "compactUnfair", title: "Compact But Unfair"},
-// { key: "neither", title: "Neither Compact Nor Fair"}
-// ];
+var groupingData = {
+    party: [[0,0,1,1,1],
+                [0,0,1,1,1],
+                [0,0,1,1,1],
+                [0,0,1,1,1],
+                [0,0,1,1,1],
+                [0,0,1,1,1],
+                [0,0,1,1,1],
+                [0,0,1,1,1],
+                [0,0,1,1,1],
+                [0,0,1,1,1]],
+    perfectRep: [[0,1,2,3,4],
+                    [0,1,2,3,4],
+                    [0,1,2,3,4],
+                    [0,1,2,3,4],
+                    [0,1,2,3,4],
+                    [0,1,2,3,4],
+                    [0,1,2,3,4],
+                    [0,1,2,3,4],
+                    [0,1,2,3,4],
+                    [0,1,2,3,4]],
+    compactUnfair: [[0,0,0,0,0],
+                    [0,0,0,0,0],
+                    [1,1,1,1,1],
+                    [1,1,1,1,1],
+                    [2,2,2,2,2],
+                    [2,2,2,2,2],
+                    [3,3,3,3,3],
+                    [3,3,3,3,3],
+                    [4,4,4,4,4],
+                    [4,4,4,4,4]],
+    neither: [[0,1,1,1,1],
+                    [0,0,0,0,1],
+                    [0,0,0,0,1],
+                    [0,2,2,2,1],
+                    [2,2,1,1,1],
+                    [2,2,3,3,3],
+                    [4,2,2,2,3],
+                    [4,4,4,4,3],
+                    [4,4,4,4,3],
+                    [4,3,3,3,3]]
+    };
 
-var groups = ["District 1", "District 2", "District 3", "District 4", "District 5"];
-
-// var headers = d3.keys(configs);
-
-var groupParty = [[0,0,1,1,1],
-            [0,0,1,1,1],
-            [0,0,1,1,1],
-            [0,0,1,1,1],
-            [0,0,1,1,1],
-            [0,0,1,1,1],
-            [0,0,1,1,1],
-            [0,0,1,1,1],
-            [0,0,1,1,1],
-            [0,0,1,1,1]];
-
-// Perfect Representation (groupingTypes[0])
-var groupPerfectRep = [[0,1,2,3,4],
-                [0,1,2,3,4],
-                [0,1,2,3,4],
-                [0,1,2,3,4],
-                [0,1,2,3,4],
-                [0,1,2,3,4],
-                [0,1,2,3,4],
-                [0,1,2,3,4],
-                [0,1,2,3,4],
-                [0,1,2,3,4]];
-
-// Compact But Unfair (groupingTypes[1])
-var groupCompactUnfair = [[0,0,0,0,0],
-                [0,0,0,0,0],
-                [1,1,1,1,1],
-                [1,1,1,1,1],
-                [2,2,2,2,2],
-                [2,2,2,2,2],
-                [3,3,3,3,3],
-                [3,3,3,3,3],
-                [4,4,4,4,4],
-                [4,4,4,4,4]];
-
-// Neither Compact Nor Unfair (groupingTypes[2])
-var groupNeither = [[0,1,1,1,1],
-                [0,0,0,0,1],
-                [0,0,0,0,1],
-                [0,2,2,2,1],
-                [2,2,1,1,1],
-                [2,2,3,3,3],
-                [4,2,2,2,3],
-                [4,4,4,4,3],
-                [4,4,4,4,3],
-                [4,3,3,3,3]];
+var districtNumbers = ["District 1", "District 2", "District 3", "District 4", "District 5"];
 
 var groupingSvg = d3.select("#districtGrouping").append("svg")
     .attr("width", width)
@@ -83,7 +69,7 @@ function districtGroupingInit() {
 
     // Add legend
     var groupLegend = groupingSvg.selectAll('.legend')
-        .data(groups)
+        .data(districtNumbers)
         .enter()
         .append('g')
         .attr('class', 'legend')
@@ -111,7 +97,7 @@ function districtGroupingInit() {
         .attr("transform", "translate(150,25)");
 
     var partyGroup = partyG.selectAll(".party")
-        .data(groupParty);
+        .data(groupingData["party"]);
 
     var partyRow = partyGroup.enter()
         .append("g")
@@ -152,21 +138,22 @@ function districtGroupingInit() {
 function districtGroupingVis() {
     // get selected Group
     selectedGroup = d3.select('input[name="groups"]:checked').property("id");
-    console.log(selectedGroup);
 
     // Square shows district    
     var districtG = groupingSvg.append("g")
         .attr("class", "districtGrouping")
         .attr("transform", "translate(150,25)");
 
-    var group = districtG.selectAll(".row")
-        .data(groupPerfectRep);
-    
-    var row = group.enter()
+    var districtRow = districtG.selectAll(".row")
+        .data(groupingData[selectedGroup], function(d) { return d; });
+
+    var row = districtRow.enter()
         .append("g")
         .attr("class", "row");
 
-    row.merge(group)
+    row.exit().remove();
+
+    row.merge(districtRow)
         .attr("height", cellHeight)
         .style("opacity",0.2)
         .transition()
@@ -177,8 +164,11 @@ function districtGroupingVis() {
         });
 
     var cell = row.selectAll(".cell")
-        .data(function(d) { return d; })
-        .enter().append("rect")
+        .data(function(d) { return d; });
+
+    cell.exit().remove();
+
+    cell.enter().append("rect")
         .attr("class", function(d, i) {
             return "column" + i;
         })
