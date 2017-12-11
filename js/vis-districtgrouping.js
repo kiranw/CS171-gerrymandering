543fffeb101b746ca8ed518a8621b98490c58cad
Kiran Wattamwar <kiranwattamwar@Kirans-MacBook-Pro.local>
2017-12-11 15:46:57 -0500

    Packing cracking vis

--- js/vis-districtgrouping.js
+++ js/vis-districtgrouping.js
@@ -61,7 +61,12 @@ var groupingSvg = d3.select("#districtGrouping").append("svg")
     .attr("width", 500)
     .attr("height", 500);
 
+var pcSvg = d3.select("#packingcracking").append("svg")
+    .attr("width", 500)
+    .attr("height", 500);
+
 districtGroupingInit();
+packingCrackingInit();
 
 
 // Draws 5x10 box representing ways to group districts
@@ -93,6 +98,37 @@ function districtGroupingInit() {
 
     // districtGroupingVis();
     districtGroupingVisInit();
+}
+
+// Draws 5x10 box representing ways to group districts
+function packingCrackingInit() {
+
+    // Add legend
+    var groupLegend = pcSvg.selectAll('.legend')
+        .data(districtNumbers)
+        .enter()
+        .append('g')
+        .attr('class', 'legend')
+        .attr('transform', function(d, i) {
+            var y = 15*i+150;
+            return 'translate(250,' + y + ')';
+        });
+
+    groupLegend.append('rect')
+        .attr('width', 15)
+        .attr('height', 15)
+        .style('fill', function(d) {
+            return groupColor(d);
+        });
+
+    groupLegend.append('text')
+        .attr("class", "legend-names")
+        .attr('x', 20)
+        .attr('y', 10)
+        .text(function(d) { return d; });
+
+    // districtGroupingVis();
+    packingCrackingVisInit();
 }
 
 
@@ -261,4 +297,129 @@ function districtGroupingVisUpdate() {
         .transition()
         .duration(300)
         .attr("opacity", 1);
+}
+
+
+
+function packingCrackingVisInit() {
+    var groupingDataById = {};
+    var keys = ["neither"];
+    keys.forEach(function(key){
+        var newList = [];
+        var counts = {};
+        groupingData[key].forEach(function(row){
+            var newRow = [];
+            row.forEach(function(d){
+                var total = 1;
+                if (d in counts){
+                    counts[d] += 1;
+                    total = counts[d];
+                }
+                else {
+                    counts[d] =1;
+                }
+                newRow.push([total,d]);
+            })
+            newList = newList.concat(newRow);
+        });
+        groupingDataById[key] = newList;
+    });
+
+    // get selected Group
+    selectedGroup = d3.select('input[name="packingcracking"]:checked').property("id");
+    currentData = groupingDataById["neither"];
+    districts = {cracking: [0,2,4], packing: [1,3]};
+
+    // Square shows district
+    var districtG = pcSvg.append("g")
+        .attr("class", "districtGrouping")
+        .attr("transform", "translate(0,25)")
+        .selectAll(".cell")
+        .data(currentData, function(d) { return d; })
+        .enter()
+        .append("rect")
+        .attr("class", function(d, i) {
+            return "pccell column" + (i%5) + " group"+d[1];
+        })
+        .attr("width", cellWidth)
+        .attr("height", cellHeight)
+        .attr("x", function(d, i) {
+            return cellWidth * (i%5);
+        })
+        .attr("y", function(d, i){
+            return cellHeight * (i - i%5)/5;
+        })
+        .attr("fill", function(d, i) {
+            return groupColor(d[1]);
+        })
+        .attr("opacity", function(d){
+            return districts[selectedGroup].indexOf(+d[1]) >= 0 ? 1 : 0.1; })
+        .on('mouseenter', dGMouseEnter)
+        .on('mouseout', dGMouseOut);
+
+    function dGMouseEnter(){
+        var groupClass = d3.select(this).attr("class").split(" ")[2];
+        d3.selectAll("."+groupClass).attr("opacity",0.5);
+    }
+
+    function dGMouseOut(){
+        var groupClass = d3.select(this).attr("class").split(" ")[2];
+        d3.selectAll("."+groupClass).attr("opacity",1);
+    }
+
+
+    // Circle shows people
+    var partyG = pcSvg.append("g")
+        .attr("class", "partyGrouping")
+        .attr("transform", "translate(0,25)");
+
+    var partyGroup = partyG.selectAll(".party")
+        .data(groupingData["party"]);
+
+    var partyRow = partyGroup.enter()
+        .append("g")
+        .attr("class", "party");
+
+    partyRow.merge(partyGroup)
+        .attr("height", cellHeight)
+        // .transition()
+        // .duration(500)
+        .style("opacity",1)
+        .attr("transform", function(d, i) {
+            return "translate(0," + (cellHeight + cellPadding) * i + ")";
+        });
+
+    var party = partyRow.selectAll(".party")
+        .data(function(d) {
+            return d;
+        })
+        .enter().append("circle")
+        .attr("class", function(d, i) {
+            return "cell" + i;
+        })
+        .attr("r", cellWidth/5)
+        .attr("cx", function(d, i) {
+            return ((cellWidth + cellPadding) * i) + (cellWidth/2);
+        })
+        .attr("cy", cellHeight/2)
+        .attr("stroke-width","3")
+        .attr("stroke", function(d,i) {
+            return (d == 0) ? "#d7301f" : "#2171b5";
+        })
+        .attr("fill", function(d,i) {
+            return (d == 0) ? "#d7301f" : "#2171b5";
+        });
+}
+
+
+function packingCrackingVisUpdate() {
+        // get selected Group
+    selectedGroup = d3.select('input[name="packingcracking"]:checked').property("id");
+    districts = {cracking: [0,2,4], packing: [1,3]};
+    console.log(selectedGroup);
+    d3.selectAll(".pccell")
+        .transition()
+        .duration(500)
+        .attr("opacity", function(d){
+            return districts[selectedGroup].indexOf(+d[1]) >= 0 ? 1 : 0.1; });
 }
